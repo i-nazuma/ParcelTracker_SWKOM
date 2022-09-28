9e6193291881cdeda37d1dd05e0c4cf79ff0bd4f
KyoHolic <81982963+KyoHolic@users.noreply.github.com>
2022-09-28 13:06:58 +0200

    init commit

---
+++ src/main/java/at/fhtw/swen3/openapitools/OpenApiGeneratorApplication.java
@@ -0,0 +1,23 @@
+package at.fhtw.swen3.openapitools;
+
+import com.fasterxml.jackson.databind.Module;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"at.fhtw.swen3.services" , "at.fhtw.swen3.services.openapitools.configuration"})
+public class OpenApiGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiGeneratorApplication.class, args);
+    }
+
+    @Bean
+    public Module jsonNullableModule() {
+        return new JsonNullableModule();
+    }
+
+}
