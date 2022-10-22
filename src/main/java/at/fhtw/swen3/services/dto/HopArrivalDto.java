5ba68be0b3aa728bda0f7242ae9a1d10e948da85
KyoHolic <adrianmkoch@hotmail.com>
2022-10-23 00:25:46 +0200

    idk anymore what I did here

--- src/main/java/at/fhtw/swen3/services/dto/HopArrivalDto.java
+++ src/main/java/at/fhtw/swen3/services/dto/HopArrivalDto.java
@@ -1,2 +1,16 @@
-package at.fhtw.swen3.services.dto;public class HopArrivalDto {
+package at.fhtw.swen3.services.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+@Getter
+@Setter
+@AllArgsConstructor
+public class HopArrivalDto {
+    private String code;
+    private String description;
+    private OffsetDateTime dateTime;
 }
